05b8d07ab0128d16c90e8469d2b82244d56c8c2f
levi.silva <levi.silva@agu.gov.br>
2026-03-28 18:58:14 -0300

    Initial scaffold for Aisly

---
+++ Aisly/AislyApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AislyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AislyTests/AislyTests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import Aisly
+
+final class AislyTests: XCTestCase {
+}
